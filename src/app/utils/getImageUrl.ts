5860dfd1b03952ebfe32675167ec0bc27d6c06e0
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-09 12:17:26 +0100

    Update getImageUrl.ts

--- src/app/utils/getImageUrl.ts
+++ src/app/utils/getImageUrl.ts
@@ -1,12 +1,19 @@
-const getImageUrl = (symbol: any) => {
-  return `https://callistobridge.netlify.app/images/${symbol}.png`;
-};
-export const getTokenLogoLink = (address: string, chainId = 820) => {
-  return address === '0xCcbf1C9E8b4f2cDF3Bfba1098b8f56f97d219D53'
+import { TOKENLIST } from '@callisto-enterprise/assetslist';
+
+// const getImageUrl = (symbol: any) => {
+//  return `https://callistobridge.netlify.app/images/${symbol}.png`;
+// };
+
+type ChainIDs = 820 | 20729;
+
+export const getTokenLogoLink = (address: string, chainId: ChainIDs = 820) => {
+  return TOKENLIST[chainId].find((entry) => entry.address.toLowerCase() === address.toLowerCase()).image;
+
+  /* return address === '0xCcbf1C9E8b4f2cDF3Bfba1098b8f56f97d219D53'
     ? 'https://app.soy.finance/images/coins/clo.png'
-    : `https://deploy-preview-25--multichain-soy-finance.netlify.app/images/coins/${chainId}/${address}.png`;
+    : `https://deploy-preview-25--multichain-soy-finance.netlify.app/images/coins/${chainId}/${address}.png`; */
 };
 
 //https://app.soy.finance/
 
-export default getImageUrl;
+// export default getImageUrl;
