80322a08c32c1670e7883100797a98f522c6f1b0
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-10 14:19:25 +0100

    Update getImageUrl.ts

--- src/app/utils/getImageUrl.ts
+++ src/app/utils/getImageUrl.ts
@@ -4,7 +4,7 @@ const getImageUrl = (symbol: any) => {
 export const getTokenLogoLink = (address: string, chainId = 820) => {
   return address === '0xCcbf1C9E8b4f2cDF3Bfba1098b8f56f97d219D53'
     ? 'https://app.soy.finance/images/coins/clo.png'
-    : `https://deploy-preview-25--multichain-soy-finance.netlify.app/images/coins/${chainId}/${address}.png`;
+    : `https://app.soy.finance/images/coins/${chainId}/${address}.png`;
 };
 
 //https://app.soy.finance/
