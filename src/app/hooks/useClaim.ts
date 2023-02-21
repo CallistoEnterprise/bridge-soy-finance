adc6ec691158788e362491f681bd6a18b8f6a2f8
fssoy <120026373+fssoy@users.noreply.github.com>
2023-02-21 11:43:32 +0100

    Add gas limit computation for claims

--- src/app/hooks/useClaim.ts
+++ src/app/hooks/useClaim.ts
@@ -1,4 +1,5 @@
 import { useCallback } from 'react';
+import { DEFAULT_GAS_LIMIT } from '../constants';
 import { getBridgeContract } from '../utils';
 import useActiveWeb3React from './useActiveWeb3React';
 
@@ -8,9 +9,12 @@ const useClaim = () => {
   const handleSimpleClaim = useCallback(
     async (respJson: any, txHash: string, fromChainId: number, sig: string) => {
       const bridgeContract = await getBridgeContract(respJson.bridge, library, account);
-      const tx = await bridgeContract.claim(respJson.token, txHash, respJson.to, respJson.value, fromChainId, sig, {
+      const gasLimit = await bridgeContract.estimateGas.claim(respJson.token, txHash, respJson.to, respJson.value, fromChainId, sig, {
         value: 0
-        // gasLimit: DEFAULT_GAS_LIMIT
+      });
+      const tx = await bridgeContract.claim(respJson.token, txHash, respJson.to, respJson.value, fromChainId, sig, {
+        value: 0,
+        gasLimit: gasLimit.add(100000)
       });
       const receipt = tx.wait();
       return receipt;
@@ -21,7 +25,7 @@ const useClaim = () => {
   const handleAdvancedClaim = useCallback(
     async (respJson: any, txHash: string, fromChainId: number, sig: string) => {
       const bridgeContract = await getBridgeContract(respJson.bridge, library, account);
-      const tx = await bridgeContract.claimToContract(
+      const gasLimit = await bridgeContract.estimateGas.claimToContract(
         respJson.token,
         txHash,
         respJson.to,
@@ -32,7 +36,20 @@ const useClaim = () => {
         sig,
         {
           value: 0
-          // gasLimit: DEFAULT_GAS_LIMIT
+        }
+      );
+      const tx = await bridgeContract.claimToContract(
+        respJson.token,
+        txHash,
+        respJson.to,
+        respJson.value,
+        fromChainId,
+        respJson.toContract,
+        respJson.data,
+        sig,
+        {
+          value: 0,
+          gasLimit: gasLimit.add(100000)
         }
       );
       const receipt = tx.wait();
