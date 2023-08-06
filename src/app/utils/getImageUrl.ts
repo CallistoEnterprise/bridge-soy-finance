b7d9f9d653d7465af0b6e359c4b93cfd1620b768
Yuriy <yuriy77k@gmail.com>
2023-08-06 17:03:00 +0300

    fix images

--- src/app/utils/getImageUrl.ts
+++ src/app/utils/getImageUrl.ts
@@ -1,7 +1,6 @@
 export const getTokenLogoLink = (address: string, chainId = 820) => {
   return address === '0xCcbf1C9E8b4f2cDF3Bfba1098b8f56f97d219D53'
-    ? 'https://app.soy.finance/images/coins/clo.png'
-    : `https://app.soy.finance/images/coins/${chainId}/${address}.png`;
+    ? 'https://bridge.soy.finance/images/coins/clo.png'
+    : `https://bridge.soy.finance/images/coins/${chainId}/${address}.png`;
 };
 
-//https://app.soy.finance/
